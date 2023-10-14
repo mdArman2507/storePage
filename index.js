d16bfbecb2e3892037a78c11ee272429a148272a
mdArman2507 <mdarman2507@gmail.com>
2023-10-14 12:00:38 +0530

    delete ,update, fileupload

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ import ProductsController from './src/controllers/product.controller.js';
 import ejsLayouts from 'express-ejs-layouts';
 import path from 'path';
 import validationMiddleware from './src/middlewares/validation.middleware.js';
+import { uploadFile } from './src/middlewares/file-upload.middleware.js';
 
 const app = express();
+
+app.use(express.static('public'));
+
 const productsController =
   new ProductsController();
 
@@ -22,7 +26,28 @@ app.get(
   '/add-product',
   productsController.getAddProduct
 );
-app.post('/', validationMiddleware, productsController.postAddProduct);
+
+app.get(
+  '/update-product/:id',
+  productsController.getUpdateProductView
+);
+
+app.post(
+  '/delete-product/:id',
+  productsController.deleteProduct
+);
+
+app.post(
+  '/',
+  uploadFile.single('imageUrl'),
+  validationMiddleware,
+  productsController.postAddProduct
+);
+
+app.post(
+  '/update-product',
+  productsController.postUpdateProduct
+);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
